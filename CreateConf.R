7a5a696e6ca54f7c5c6317b90476bfe4187596ec
Thomas Yu <thomasyu888@gmail.com>
2015-06-11 13:40:55 -0700

    Update

--- CreateConf.R
+++ CreateConf.R
@@ -1,17 +1,21 @@
+library(tools)
+file_ext(files[1])
 total <- list.files(".. Your file directory containing bigwig/BAM files..")
-
+files <- list.files("/Library/WebServer/Documents/")
 config(total, "human")
+files
+tfiles
 config <- function(files, directory) {
   conf <- file("tracks.conf")
   ##Don't want the files with .bai in it
-  files <- files[!files%in%".bai"]
+  tfiles <- files[file_ext(files) %in% c("bw","bam")]
   #sapply through files, -> if bigwig, then do formatting for bigwig files (ifelse) else do BAM
-  lines <- sapply(files, function(i) { 
+  lines <- sapply(tfiles, function(i) { 
     track <- paste("[tracks.", which(i==files), "]",sep="")
     class <- paste("storeClass = JBrowse/Store/SeqFeature/", ifelse(grepl("bw",i),"BigWig","BAM"),sep="")
     plot <- paste("type = JBrowse/View/Track/",ifelse(grepl("bw",i),"Wiggle/XYPlot","Alignments2"),sep="")
     uniq <- ifelse(grepl("bw",i),"autoscale = clipped_global",
-                   paste("baiUrlTemplate = ../../raw/",directory,"/",i,".bai",sep=""))
+                   paste("baiUrlTemplate = ../../raw/",directory,"/",i,".bai",sep="")) ##add .bai if bam file
     cat <- paste("metadata.category = ",directory,ifelse(grepl("bw",i),"/ Coverage","/ Reads"),sep="")
     temp <- paste("urlTemplate = ../../raw/",directory,"/",i,sep="")
     key <- paste("key = ",i,sep="")
